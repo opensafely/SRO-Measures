503793884a3a542c8de0ef0f85b8fb6970a0a7ed
LFISHER7 <lhf282@live.co.uk>
2021-02-15 13:02:28 +0000

    updated study definition

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -2,3 +2,8 @@ from cohortextractor import (
     codelist,
     codelist_from_csv,
 )
+
+
+holder_codelist = codelist_from_csv("codelists/opensafely-structured-medication-review-nhs-england.csv",
+                  system="snomed",
+                  column="code",)
--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -5,20 +5,65 @@ from cohortextractor import (
     patients,
     codelist_from_csv,
     codelist,
-    filter_codes_by_category,
-    combine_codelists,
+    Measure
 )
 
 # Import codelists
 
 from codelists import *
+from datetime import date
 
 
-
-    
+start_date = "2020-12-07"
+end_date = date.today().isoformat()
 # Specifiy study definition
 
 study = StudyDefinition(
-  
+    default_expectations={
+        "date": {"earliest": start_date, "latest": end_date},
+        "rate": "exponential_increase",
+        "incidence": 0.1,
+    },
     
+    population=patients.satisfying(
+        """
+        registered AND
+        (NOT died)
+        """
+    ),
+
+    registered = patients.registered_as_of(
+        start_date,
+        return_expectations={"incidence": 0.9},
+        ),
+
+    died = patients.died_from_any_cause(
+        on_or_before=end_date,
+        returning="binary_flag",
+        return_expectations={"incidence": 0.1}
+    ),
+
+    practice=patients.registered_practice_as_of(
+        start_date,
+        returning="pseudo_id",
+        return_expectations={"int" : {"distribution": "normal", "mean": 25, "stddev": 5}, "incidence" : 0.5}
+    ),
+  
+
+    sentinel_measure_x = patients.with_these_clinical_events(
+        codelist = holder_codelist,
+        between=[start_date, end_date],
+        returning="binary_flag",
+        return_expectations={"incidence": 0.1}
+    )
 )
+
+
+measures = [
+    Measure(
+        id="sentinel_measure_x",
+        numerator="sentinel_measure_x",
+        denominator="population",
+        group_by="population"
+    )
+]
