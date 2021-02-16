08696a92aff4765bc740ef86444b8d9d7a9c491c
LFISHER7 <lhf282@live.co.uk>
2021-02-16 10:26:29 +0000

    patient count script

---
+++ analysis/get_patients_counts.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import os
+import json
+import numpy as np
+
+sentinel_measures = ["systolic_bp", "qrisk", "cholesterol", "bilirubin",
+                     "serum_tsh", "rbc_fbc", "hba1c", "serum_sodium", "asthma"]
+
+sentinel_measures_test  = ['sentinel_measure_x']
+patient_counts_dict = {}
+patient_dict = {}
+
+
+for file in os.listdir('output'):
+    if file.startswith('input'):
+        df = pd.read_csv(os.path.join('output', file))
+
+        for measure in sentinel_measures_test:
+            df_subset = df[df[measure]==1]
+            # get unique patients
+            patients = list(df_subset['patient_id'])
+
+            if measure not in patient_dict:
+                #create key
+                patient_dict[measure] = patients
+            
+            else:
+                patient_dict[measure].extend(patients)
+
+
+
+for (key, value) in patient_dict.items():
+    #get unique patients
+    unique_patients = len(np.unique(patient_dict[key]))
+
+    patient_counts_dict[key] = unique_patients
+      
+
+
+with open('output/patient_count.json', 'w') as f:
+    json.dump({"num_patients": patient_counts_dict}, f)
