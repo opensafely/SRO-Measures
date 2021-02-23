d7603359aa3e1749f0ee2866326fc2e3d3b55cee
LFISHER7 <lhf282@live.co.uk>
2021-02-23 10:17:59 +0000

    fix notebook

--- notebooks/sentinel_measures.py
+++ notebooks/sentinel_measures.py
@@ -39,24 +39,6 @@ from utilities import *
 
 # +
 
-
-HTML('''<script>
-code_show=true; 
-function code_toggle() {
- if (code_show){
- $('div.input').hide();
- } else {
- $('div.input').show();
- }
- code_show = !code_show
-} 
-$( document ).ready(code_toggle);
-</script>
-The raw code for this IPython notebook is by default hidden for easier reading.
-To toggle on/off the raw code, click <a href="javascript:code_toggle()">here</a>.''')
-
-# +
-
 HTML('''<script>
 code_show_err=false; 
 function code_toggle_err() {
