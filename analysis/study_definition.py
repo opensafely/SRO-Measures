a9384f0d00b94286740b8f59cc090fbf6b79e0f9
LFISHER7 <lhf282@live.co.uk>
2021-03-01 18:30:55 +0000

    hide study def variables not yet coded for

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -54,21 +54,7 @@ study = StudyDefinition(
     ),
   
 
-    systolic_bp = patients.with_these_clinical_events(
-        codelist = crd_codelist,
-        between=[start_date, end_date],
-        returning="binary_flag",
-        return_expectations={"incidence": 0.5}
-    ),
-
-    systolic_bp_event_code=patients.with_these_clinical_events(
-        codelist=crd_codelist,
-        between=[start_date, end_date],
-        returning="code",
-        return_expectations={"category": {
-            "ratios": {"23E5.": 0.6, "663K.": 0.2, "7450.": 0.2}}, }
-    ),
-
+  
     qrisk2=patients.with_these_clinical_events(
         codelist=qrisk_codelist,
         between=[start_date, end_date],
@@ -84,96 +70,7 @@ study = StudyDefinition(
             "ratios": {str(1085871000000105): 0.6, str(450759008): 0.2, str(718087004): 0.2}}, }
     ),
 
-    serum_cholesterol=patients.with_these_clinical_events(
-        codelist=crd_codelist,
-        between=[start_date, end_date],
-        returning="binary_flag",
-        return_expectations={"incidence": 0.5}
-    ),
-
-    serum_cholesterol_event_code=patients.with_these_clinical_events(
-        codelist=crd_codelist,
-        between=[start_date, end_date],
-        returning="code",
-        return_expectations={"category": {
-            "ratios": {"23E5.": 0.6, "663K.": 0.2, "7450.": 0.2}}, }
-    ),
-
-    serum_bilirubin=patients.with_these_clinical_events(
-        codelist=crd_codelist,
-        between=[start_date, end_date],
-        returning="binary_flag",
-        return_expectations={"incidence": 0.5}
-    ),
-
-    serum_bilirubin_event_code=patients.with_these_clinical_events(
-        codelist=crd_codelist,
-        between=[start_date, end_date],
-        returning="code",
-        return_expectations={"category": {
-            "ratios": {"23E5.": 0.6, "663K.": 0.2, "7450.": 0.2}}, }
-    ),
-
-    serum_tsh=patients.with_these_clinical_events(
-        codelist=crd_codelist,
-        between=[start_date, end_date],
-        returning="binary_flag",
-        return_expectations={"incidence": 0.5}
-    ),
-
-    serum_tsh_event_code=patients.with_these_clinical_events(
-        codelist=crd_codelist,
-        between=[start_date, end_date],
-        returning="code",
-        return_expectations={"category": {
-            "ratios": {"23E5.": 0.6, "663K.": 0.2, "7450.": 0.2}}, }
-    ),
-
-    rbc=patients.with_these_clinical_events(
-        codelist=crd_codelist,
-        between=[start_date, end_date],
-        returning="binary_flag",
-        return_expectations={"incidence": 0.5}
-    ),
-
-    rbc_event_code=patients.with_these_clinical_events(
-        codelist=crd_codelist,
-        between=[start_date, end_date],
-        returning="code",
-        return_expectations={"category": {
-            "ratios": {"23E5.": 0.6, "663K.": 0.2, "7450.": 0.2}}, }
-    ),
-
-    hba1c=patients.with_these_clinical_events(
-        codelist=crd_codelist,
-        between=[start_date, end_date],
-        returning="binary_flag",
-        return_expectations={"incidence": 0.5}
-    ),
-
-    hba1c_event_code=patients.with_these_clinical_events(
-        codelist=crd_codelist,
-        between=[start_date, end_date],
-        returning="code",
-        return_expectations={"category": {
-            "ratios": {"23E5.": 0.6, "663K.": 0.2, "7450.": 0.2}}, }
-    ),
-
-    serum_sodium=patients.with_these_clinical_events(
-        codelist=crd_codelist,
-        between=[start_date, end_date],
-        returning="binary_flag",
-        return_expectations={"incidence": 0.5}
-    ),
-
-    serum_sodium_event_code=patients.with_these_clinical_events(
-        codelist=crd_codelist,
-        between=[start_date, end_date],
-        returning="code",
-        return_expectations={"category": {
-            "ratios": {"270442000": 0.6, "663K.": 0.2, "7450.": 0.2}}, }
-    ),
-
+  
     asthma=patients.with_these_clinical_events(
         codelist=asthma_codelist,
         between=[start_date, end_date],
@@ -207,20 +104,7 @@ study = StudyDefinition(
 
 
 measures = [
-    Measure(
-        id="systolic_bp",
-        numerator="systolic_bp",
-        denominator="population",
-        group_by=["practice", "systolic_bp_event_code"]
-    ),
-
-    Measure(
-        id="systolic_bp_practice_only",
-        numerator="systolic_bp",
-        denominator="population",
-        group_by=["practice"]
-    ),
-
+   
     Measure(
         id="qrisk2",
         numerator="qrisk2",
@@ -235,90 +119,7 @@ measures = [
         group_by=["practice"]
     ),
 
-    Measure(
-        id="serum_cholesterol",
-        numerator="serum_cholesterol",
-        denominator="population",
-        group_by=["practice", "serum_cholesterol_event_code"]
-    ),
-
-    Measure(
-        id="serum_cholesterol_practice_only",
-        numerator="serum_cholesterol",
-        denominator="population",
-        group_by=["practice"]
-    ),
-
-    Measure(
-        id="serum_bilirubin",
-        numerator="serum_bilirubin",
-        denominator="population",
-        group_by=["practice", "serum_bilirubin_event_code"]
-    ),
-
-    Measure(
-        id="serum_bilirubin_practice_only",
-        numerator="serum_bilirubin",
-        denominator="population",
-        group_by=["practice"]
-    ),
-
-    Measure(
-        id="serum_tsh",
-        numerator="serum_tsh",
-        denominator="population",
-        group_by=["practice", "serum_tsh_event_code"]
-    ),
-
-    Measure(
-        id="serum_tsh_practice_only",
-        numerator="serum_tsh",
-        denominator="population",
-        group_by=["practice"]
-    ),
-
-    Measure(
-        id="rbc",
-        numerator="rbc",
-        denominator="population",
-        group_by=["practice", "rbc_event_code"]
-    ),
-
-    Measure(
-        id="rbc_practice_only",
-        numerator="rbc",
-        denominator="population",
-        group_by=["practice"]
-    ),
-
-    Measure(
-        id="hba1c",
-        numerator="hba1c",
-        denominator="population",
-        group_by=["practice", "hba1c_event_code"]
-    ),
-
-    Measure(
-        id="hba1c_practice_only",
-        numerator="hba1c",
-        denominator="population",
-        group_by=["practice"]
-    ),
-
-    Measure(
-        id="serum_sodium",
-        numerator="serum_sodium",
-        denominator="population",
-        group_by=["practice", "serum_sodium_event_code"]
-    ),
-
-    Measure(
-        id="serum_sodium_practice_only",
-        numerator="serum_sodium",
-        denominator="population",
-        group_by=["practice"]
-    ),
-
+   
     Measure(
         id="asthma",
         numerator="asthma",
