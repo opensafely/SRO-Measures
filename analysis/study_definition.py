5710c6f53a55106994dc287e75dcc3b255ed4d26
LFISHER7 <lhf282@live.co.uk>
2021-04-28 16:43:07 +0100

    Add demographics to study def

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -34,18 +34,98 @@ study = StudyDefinition(
         """
         registered AND
         (NOT died)
-        """
-    ),
+        """,
 
-    registered = patients.registered_as_of(
+        registered = patients.registered_as_of(
         "index_date",
         return_expectations={"incidence": 0.9},
         ),
 
-    died = patients.died_from_any_cause(
+        died = patients.died_from_any_cause(
         on_or_before="index_date",
         returning="binary_flag",
         return_expectations={"incidence": 0.1}
+        ),
+    ),
+
+    
+
+
+    age=patients.age_as_of(
+        "index_date",
+        return_expectations={
+            "rate": "universal",
+            "int": {"distribution": "population_ages"},
+        },
+    ),
+
+    age_band=patients.categorised_as(
+        {
+            "missing": "DEFAULT",
+            "0-19": """ age >= 0 AND age < 20""",
+            "20-29": """ age >=  20 AND age < 30""",
+            "30-39": """ age >=  30 AND age < 40""",
+            "40-49": """ age >=  40 AND age < 50""",
+            "50-59": """ age >=  50 AND age < 60""",
+            "60-69": """ age >=  60 AND age < 70""",
+            "70-79": """ age >=  70 AND age < 80""",
+            "80+": """ age >=  80 AND age < 120""",
+        },
+        return_expectations={
+            "rate": "universal",
+            "category": {
+                "ratios": {
+                    "0-19": 0.125,
+                    "20-29": 0.125,
+                    "30-39": 0.125,
+                    "40-49": 0.125,
+                    "50-59": 0.125,
+                    "60-69": 0.125,
+                    "70-79": 0.125,
+                    "80+": 0.125,
+                }
+            },
+        },
+
+    ),
+
+
+    sex=patients.sex(
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"M": 0.49, "F": 0.5, "U": 0.01}},
+        }
+    ),
+
+    region=patients.registered_practice_as_of(
+        "index_date",
+        returning="nuts1_region_name",
+        return_expectations={"category": {"ratios": {
+            "North East": 0.1,
+            "North West": 0.1,
+            "Yorkshire and the Humber": 0.1,
+            "East Midlands": 0.1,
+            "West Midlands": 0.1,
+            "East of England": 0.1,
+            "London": 0.2,
+            "South East": 0.2, }}}
+    ),
+    
+    imd=patients.address_as_of(
+        "index_date",
+        returning="index_of_multiple_deprivation",
+        round_to_nearest=100,
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"100": 0.2, "200": 0.2, "300": 0.2, "400": 0.2, "500": 0.2}},
+        },
+    ),
+
+    learning_disability=patients.with_these_clinical_events(
+        ld_codes,
+        on_or_before="index_date",
+        returning="binary_flag",
+        return_expectations={"incidence": 0.01, },
     ),
 
     practice=patients.registered_practice_as_of(
