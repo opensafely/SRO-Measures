7f151d293a8af405cbf8687ff662fbff7cf952b6
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2021-11-30 14:59:00 +0000

    update measure naming

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -7,6 +7,9 @@ from cohortextractor import (
     codelist,
     Measure
 )
+sentinel_measures = ["qrisk2", "asthma", "copd", "sodium", "cholesterol", "alt", "tsh", "rbc", 'hba1c', 'systolic_bp', 'medication_review']
+demographics = ['region', 'age_band', 'imd', 'sex', 'learning_disability', 'ethnicity']
+
 
 # Import codelists
 
@@ -323,161 +326,23 @@ study = StudyDefinition(
    
 )
 
+measures = []
 
-measures = [
-    
-    Measure(
-        id="systolic_bp",
-        numerator="systolic_bp",
+for measure in sentinel_measures:
+    measures.extend([
+        Measure(
+        id=f"{measure}_rate",
+        numerator=measure,
         denominator="population",
-        group_by=["practice", "systolic_bp_event_code"]
+        group_by=["practice", f"{measure}_event_code"]
     ),
 
-    Measure(
-        id="systolic_bp_practice_only",
-        numerator="systolic_bp",
-        denominator="population",
-        group_by=["practice"]
-    ),
-
-    Measure(
-        id="qrisk2",
-        numerator="qrisk2",
-        denominator="population",
-        group_by=["practice", "qrisk2_event_code"]
-    ),
-
-    Measure(
-        id="qrisk2_practice_only",
-        numerator="qrisk2",
+        Measure(
+        id=f"{measure}_practice_only_rate",
+        numerator=measure,
         denominator="population",
         group_by=["practice"]
-    ),
+    )
+    ])
 
-    Measure(
-        id="cholesterol",
-        numerator="cholesterol",
-        denominator="population",
-        group_by=["practice", "cholesterol_event_code"]
-    ),
-
-    Measure(
-        id="cholesterol_practice_only",
-        numerator="cholesterol",
-        denominator="population",
-        group_by=["practice"]
-    ),
    
-    Measure(
-        id="alt",
-        numerator="alt",
-        denominator="population",
-        group_by=["practice", "alt_event_code"]
-    ),
-
-    Measure(
-        id="alt_practice_only",
-        numerator="alt",
-        denominator="population",
-        group_by=["practice"]
-    ),
-
-    Measure(
-        id="tsh",
-        numerator="tsh",
-        denominator="population",
-        group_by=["practice", "tsh_event_code"]
-    ),
-
-    Measure(
-        id="tsh_practice_only",
-        numerator="tsh",
-        denominator="population",
-        group_by=["practice"]
-    ),
-
-    Measure(
-        id="rbc",
-        numerator="rbc",
-        denominator="population",
-        group_by=["practice", "rbc_event_code"]
-    ),
-
-    Measure(
-        id="rbc_practice_only",
-        numerator="rbc",
-        denominator="population",
-        group_by=["practice"]
-    ),
-
-    Measure(
-        id="hba1c",
-        numerator="hba1c",
-        denominator="population",
-        group_by=["practice", "hba1c_event_code"]
-    ),
-
-    Measure(
-        id="hba1c_practice_only",
-        numerator="hba1c",
-        denominator="population",
-        group_by=["practice"]
-    ),
-
-    Measure(
-        id="sodium",
-        numerator="sodium",
-        denominator="population",
-        group_by=["practice", "sodium_event_code"]
-    ),
-
-    Measure(
-        id="sodium_practice_only",
-        numerator="sodium",
-        denominator="population",
-        group_by=["practice"]
-    ),
-
-    Measure(
-        id="asthma",
-        numerator="asthma",
-        denominator="population",
-        group_by=["practice", "asthma_event_code"]
-    ),
-
-    Measure(
-        id="asthma_practice_only",
-        numerator="asthma",
-        denominator="population",
-        group_by=["practice"]
-    ),
-
-    Measure(
-        id="copd",
-        numerator="copd",
-        denominator="population",
-        group_by=["practice", "copd_event_code"]
-    ),
-
-    Measure(
-        id="copd_practice_only",
-        numerator="copd",
-        denominator="population",
-        group_by=["practice"]
-    ),
-    
-    Measure(
-        id="medication_review",
-        numerator="medication_review",
-        denominator="population",
-        group_by=["practice", "medication_review_event_code"]
-    ),
-
-    Measure(
-        id="medication_review_practice_only",
-        numerator="medication_review",
-        denominator="population",
-        group_by=["practice"]
-    ),
-]
-
