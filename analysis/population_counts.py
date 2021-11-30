9978a107d171fa950470a31bf8033d8de60bc9aa
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2021-11-30 14:27:01 +0000

    update population_counts

--- analysis/population_counts.py
+++ analysis/population_counts.py
@@ -3,6 +3,8 @@ import pandas as pd
 from datetime import datetime
 import os
 from collections import Counter
+from utilities import *
+
 
 # get total number of practices by region to look at backend coverage
 # source: https://digital.nhs.uk/data-and-information/publications/statistical/patients-registered-at-a-gp-practice/metadata#gp-reg-pat-prac-map
@@ -11,46 +13,34 @@ num_practices_region = practice_df.groupby(by='COMM_REGION_NAME')['PRACTICE_CODE
 num_practices_region.to_csv('output/practice_region_total_count.csv')
 
 dates_list = []
-path_list = []
-for file in os.listdir('output'):
-    if file.startswith('input_2'):
-        date = file.split('_')[-1][:-8]
-        datetime_object = pd.to_datetime(date)
-        dates_list.append(datetime_object)
-        path_list.append(file)
-        
-
-sorted_paths = [x for _,x in sorted(zip(dates_list,path_list))]
-
-
 
 full_df = pd.read_feather(os.path.join('output', 'input_2019-01-01.feather'))
 full_df = full_df.set_index('patient_id')
 
-for file in sorted_paths:
-    df = pd.read_feather(os.path.join('output', file))
-    df = df.set_index('patient_id')
-    #update existing values in full_df
-    full_df.update(df)
+
+for file in sorted(os.listdir('output')):
+    if match_input_files(file):
+
+        date = get_date_input_file(file)
+        datetime_object = pd.to_datetime(date)
+        dates_list.append(datetime_object)
+        
+
+        df = pd.read_feather(os.path.join('output', file))
+        df = df.set_index('patient_id')
+        #update existing values in full_df
+        full_df.update(df)
+        
+        #add new rows to full_df
+        existing_id = list(full_df.index) 
+        df = df[~df.index.isin(existing_id)]
     
-    #add new rows to full_df
-    existing_id = list(full_df.index) 
-    df = df[~df.index.isin(existing_id)]
-  
-    
-    full_df = pd.concat([full_df, df])
+        
+        full_df = pd.concat([full_df, df])
 
     
-def calculate_imd_group(df):
-    imd_column = pd.to_numeric(df["imd"])
-    df["imd"] = pd.qcut(imd_column, q=5,duplicates="drop", labels=['Most deprived', '2', '3', '4', 'Least deprived'])      
-    
-    
-    
-    return df
 
 full_df = calculate_imd_group(full_df)
-
 
 unique_practices = full_df['practice'].unique()
 
