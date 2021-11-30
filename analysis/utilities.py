77c4381200caa0080084f90b3a937d5d304a8d57
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2021-11-30 11:59:13 +0000

    add match_input_files function

--- analysis/utilities.py
+++ analysis/utilities.py
@@ -1,6 +1,6 @@
 import json
 from pathlib import Path
-
+import re
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
@@ -611,7 +611,10 @@ def display_changes(baseline, values, changes, dates):
             f"Change in median from April 2019 ({baseline}) - {date} ({value}): ({change}%)"
         ))
 
-
+def match_input_files(file: str) -> bool:
+    """Checks if file name has format outputted by cohort extractor"""
+    pattern = r"^input_20\d\d-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])\.feather"
+    return True if re.match(pattern, file) else False
 
 
 
